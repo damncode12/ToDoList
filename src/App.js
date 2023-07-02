f19415ac9639f44e84bd5a923d64b54d0a406f68
Ayushi Dwivedi <97382798+damncode12@users.noreply.github.com>
2023-07-02 22:22:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import Todo from './components/todo/todo';
 import './App.css';
-import ToDoList from './components/ToDoList'
 
 function App() {
   return (
-    <div className="To_Do_List container">
-      <ToDoList></ToDoList>
+    <div className="App">
+      <header className="App-header">
+       <Todo />
+      </header>
     </div>
   );
 }
